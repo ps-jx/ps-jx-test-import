7c4ae954b36e3663e72fcb9f8a52da3a608f31cc
Andrew Morgan <andrewmo@backbase.com>
2020-05-01 13:57:26 +0200

    Initial import

---
+++ src/main/java/com/example/psjxtestimport/PsJxTestImportApplication.java
@@ -0,0 +1,13 @@
+package com.example.psjxtestimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsJxTestImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsJxTestImportApplication.class, args);
+	}
+
+}
